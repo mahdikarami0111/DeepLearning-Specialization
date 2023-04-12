0ad38f5583396670b321452da7fc7c68d980c06e
Mahdi <mahdikarami1711@aut.ac.ir>
2023-04-12 15:25:56 +0430

    model complete

--- LogisticRegression.py
+++ LogisticRegression.py
@@ -1,3 +1,4 @@
+import numpy
 import numpy as np
 import matplotlib.pyplot as plt
 from PIL import Image
@@ -27,20 +28,22 @@ def sigmoid(z):
 
 
 def initialize(dim):
-    w = np.zeros((dim, 1))
+    w = np.random.randn(dim, 1) * 0.01
     b = 0
 
     return w, b
 
 
 def propagate(w, b, X, Y):
-    Z = np.dot(w.T, b) + b
-    A = sigmoid(Z)
     m = X.shape[1]
+    dropOut = int(X.shape[0]//10)
+
+    Z = np.dot(w.T, X) + b
+    A = sigmoid(Z)
     cost = 1/m * np.sum((-(Y * np.log(A) + (1-Y) * np.log(1-A))))
     dz = A - Y
     dw = np.dot(X, dz.T)/m
-    db = np.sum(Z)/m
+    db = np.sum(dz)/m
 
     assert (dw.shape == w.shape)
     assert (db.dtype == float)
@@ -51,6 +54,71 @@ def propagate(w, b, X, Y):
              "db": db}
 
     return grads, cost
+
+
+def optimize(w, b, X, Y, num_iterations, learning_rate, print_cost = False):
+    costs = []
+    for i in range(num_iterations):
+        gradient, cost = propagate(w, b, X, Y)
+        w = w - learning_rate * gradient["dw"]
+        b = b - learning_rate * gradient["db"]
+
+        dw = gradient["dw"]
+        db = gradient["db"]
+
+        if i % 100 == 0:
+            costs.append(cost)
+
+            # Print the cost every 100 training iterations
+        if print_cost and i % 100 == 0:
+            print("Cost after iteration %i: %f" % (i, cost))
+
+    params = {"w": w,
+              "b": b}
+
+    grads = {"dw": dw,
+             "db": db}
+
+    return params, grads, costs
+
+
+def predict(w, b, X):
+    m = X.shape[1]
+    Y_prediction = np.zeros((1, m))
+    w = w.reshape(X.shape[0], 1)
+
+    Z = np.dot(w.T, X) + b
+    A = sigmoid(Z) - 0.00000001
+    Y_prediction = (A >= 0.5) * 1.0
+
+    assert (Y_prediction.shape == (1, m))
+
+    return Y_prediction
+
+
+def model(X_train, Y_train, X_test, Y_test, num_iterations = 2000, learning_rate = 0.5, print_cost = False):
+    w, b = initialize(X_train.shape[0])
+
+    params, gradient, costs = optimize(w, b, X_train, Y_train, num_iterations, learning_rate, print_cost)
+
+    w = params["w"]
+    b = params["b"]
+
+    Y_prediction_test = predict(w, b, X_test)
+    Y_prediction_train = predict(w, b, X_train)
+
+    print("train accuracy: {} %".format(100 - np.mean(np.abs(Y_prediction_train - Y_train)) * 100))
+    print("test accuracy: {} %".format(100 - np.mean(np.abs(Y_prediction_test - Y_test)) * 100))
+
+    d = {"costs": costs,
+         "Y_prediction_test": Y_prediction_test,
+         "Y_prediction_train": Y_prediction_train,
+         "w": w,
+         "b": b,
+         "learning_rate": learning_rate,
+         "num_iterations": num_iterations}
+
+    return d
 
 
 
@@ -67,8 +135,24 @@ test_set_x_flatten = test_set_x_orig.reshape(test_set_x_orig.shape[0], -1).T
 train_set_x = train_set_x_flatten/255.
 test_set_x = test_set_x_flatten/255.
 
-w, b, X, Y = np.array([[1.],[2.]]), 2., np.array([[1.,2.,-1.],[3.,4.,-3.2]]), np.array([[1,0,1]])
-grads, cost = propagate(w, b, X, Y)
-print ("dw = " + str(grads["dw"]))
-print ("db = " + str(grads["db"]))
-print ("cost = " + str(cost))
+# w, b, X, Y = np.array([[1.],[2.]]), 2., np.array([[1.,2.,-1.],[3.,4.,-3.2]]), np.array([[1,0,1]])
+# params, grads, costs = optimize(w, b, X, Y, num_iterations= 100, learning_rate = 0.009, print_cost = False)
+#
+# print ("w = " + str(params["w"]))
+# print ("b = " + str(params["b"]))
+# print ("dw = " + str(grads["dw"]))
+# print ("db = " + str(grads["db"]))
+# plt.plot(costs)
+# plt.show()
+# w = np.array([[0.1124579],[0.23106775]])
+# b = -0.3
+# X = np.array([[1.,-1.1,-3.2],[1.2,2.,0.1]])
+# print ("predictions = " + str(predict(w, b, X)))
+
+d = model(train_set_x, train_set_y, test_set_x, test_set_y, num_iterations = 2000, learning_rate = 0.005, print_cost = False)
+costs = np.squeeze(d['costs'])
+plt.plot(costs)
+plt.ylabel('cost')
+plt.xlabel('iterations (per hundreds)')
+plt.title("Learning rate =" + str(d["learning_rate"]))
+plt.show()
